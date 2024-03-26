31b553d0eb94f0862f0c1117475acef269813001
Lucas Hogg <73367876+lucashogg@users.noreply.github.com>
2024-03-26 08:03:43 -0400

    Start game logic

--- scripts/script.js
+++ scripts/script.js
@@ -8,13 +8,41 @@ const player = (symbol) => {
 // Initialize board
 const gameboard = () => {
     const board = ['', '', '', '', '', '', '', '', ''];
+    const winningCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
 
-    return { board };
+    return { board, winningCombos };
 }
 
 const gameController = () => {
     const player1 = player('x');
     const player2 = player('o');
+    const gameCount = 0;
 
-    return { player1, player2 };
+    const board = gameboard();
+
+    let currentPlayer = player1;
+    const switchCurrentPlayer = () => {
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+    }
+    const getCurrentPlayer = () => currentPlayer;
+
+    const printNewRound = () => {
+        console.log(`It's ${getCurrentPlayer().symbol}'s turn.`);
+    }
+
+    const playRound = () => {
+        printNewRound();
+
+    }
+
+    return { playRound };
 }
